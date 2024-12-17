93c4c3825d4a482678f66dc18aacd2104da01f27
Ryan DECIAN <ryan.decian.propgmail.com>
2024-12-17 13:25:10 +0100

    Création du composant Nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+
+
+function Nav() {
+    return (
+        <>
+        <button>Noël</button>
+        <button>Nouvel an</button>
+        <button>Noël prochain</button>
+        </>
+    )
+}
+
+export default Nav;
---
+++ src/pages/PageHome.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import PageChristmasDay from "./PageChristmasDay";
+
+
+function PageHome() {
+    return (
+        <div>
+            <PageChristmasDay />
+        </div>
+    )
+}
+
+export default PageHome;
